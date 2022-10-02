a70f046753f283f37ba6404e6c5729c368babd83
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-10-02 12:09:58 +0200

    cambios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { ImageModule } from 'primeng/image';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
     MenuComponent
   ],
   imports: [
-    PrimeNgModule
+    PrimeNgModule,
+    ImageModule
   ]
 })
 export class SharedModule { }
